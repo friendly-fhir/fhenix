f2d9e224821ea4522fefdd9acf0e8738cce95e61
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-07-10 17:47:21 -0400

    Add definitional conformance model
    Eventually the goal is to fully replace the existing `model` package
    with something that is easier to use and test. As an incremental step
    there, we need a way that can be used to define conformance
    types from their JSON definitions.
    
    This introduces the initial version which does precisely that; it
    parses the JSON definitions and creates the appropriate resource types
    that are part of the conformance model.

---
+++ model/conformance/definition/definition.go
@@ -0,0 +1,74 @@
+/*
+Package definition provides the raw definitions for FHIR data types.
+*/
+package definition
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	fhir "github.com/friendly-fhir/go-fhir/r4/core"
+	"github.com/friendly-fhir/go-fhir/r4/core/resources/codesystem"
+	"github.com/friendly-fhir/go-fhir/r4/core/resources/structuredefinition"
+	"github.com/friendly-fhir/go-fhir/r4/core/resources/valueset"
+)
+
+type StructureDefinition = structuredefinition.StructureDefinition
+type ValueSets = valueset.ValueSet
+type CodeSystem = codesystem.CodeSystem
+
+// Canonical is an interface that represents a FHIR definition that has a
+// canonical URL.
+type Canonical interface {
+	GetURL() *fhir.URI
+}
+
+var (
+	_ Canonical = (*StructureDefinition)(nil)
+	_ Canonical = (*ValueSets)(nil)
+	_ Canonical = (*CodeSystem)(nil)
+)
+
+// FromFile reads a canonical definition from a file path.
+func FromFile(path string) (Canonical, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return FromReader(file)
+}
+
+// FromReader returns a canonical definition from a reader.
+func FromReader(reader io.Reader) (Canonical, error) {
+	bytes, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return FromJSON(bytes)
+}
+
+// FromJSON returns a canonical definition from a JSON byte definition.
+func FromJSON(data []byte) (Canonical, error) {
+	var resourceType struct {
+		ResourceType string `json:"resourceType"`
+	}
+	if err := json.Unmarshal(data, &resourceType); err != nil {
+		return nil, err
+	}
+	var result Canonical
+	switch resourceType.ResourceType {
+	case "StructureDefinition":
+		result = &StructureDefinition{}
+	case "ValueSet":
+		result = &ValueSets{}
+	case "CodeSystem":
+		result = &CodeSystem{}
+	default:
+		return nil, fmt.Errorf("unknown resource type %q", resourceType.ResourceType)
+	}
+	err := json.Unmarshal(data, result)
+	return result, err
+}
---
+++ model/conformance/definition/definition_test.go
@@ -0,0 +1,76 @@
+package definition_test
+
+import (
+	"encoding/json"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/friendly-fhir/fhenix/model/conformance/definition"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func mustParseJSON[T any](t *testing.T, bytes []byte) *T {
+	t.Helper()
+
+	var out T
+	if err := json.Unmarshal(bytes, &out); err != nil {
+		t.Fatalf("failed to unmarshal JSON: %v", err)
+	}
+	return &out
+}
+
+func mustReadJSON[T any](t *testing.T, path string) *T {
+	t.Helper()
+
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+	return mustParseJSON[T](t, bytes)
+}
+
+func TestReadFile(t *testing.T) {
+	testCases := []struct {
+		name    string
+		path    string
+		want    any
+		wantErr error
+	}{
+		{
+			name: "structure definition",
+			path: "testdata/structure-definition.json",
+			want: mustReadJSON[definition.StructureDefinition](t, "testdata/structure-definition.json"),
+		}, {
+			name: "value set",
+			path: "testdata/value-set.json",
+			want: mustReadJSON[definition.ValueSets](t, "testdata/value-set.json"),
+		}, {
+			name: "code system",
+			path: "testdata/code-system.json",
+			want: mustReadJSON[definition.CodeSystem](t, "testdata/code-system.json"),
+		}, {
+			name:    "invalid path",
+			path:    "testdata/invalid.json",
+			wantErr: fs.ErrNotExist,
+		}, {
+			name:    "not a resource",
+			path:    "testdata/not-resource.json",
+			wantErr: cmpopts.AnyError,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := definition.FromFile(tc.path)
+
+			if got, want := err, tc.wantErr; !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+				t.Fatalf("FromFile(%q) = error %v, want %v", tc.path, got, want)
+			}
+			if got, want := got, tc.want; !cmp.Equal(got, want) {
+				t.Fatalf("FromFile(%q) = %v, want %v", tc.path, got, want)
+			}
+		})
+	}
+}
