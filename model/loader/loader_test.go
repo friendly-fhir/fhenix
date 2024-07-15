f901b0bd2807f7b7b0a5bb9c41b1c757a4ce550d
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-07-14 20:45:54 -0400

    Add model loader package
    This is largely an initial (but tested) draft of a new mechanism for
    generating the conformance module from a registry.Cache.

---
+++ model/loader/loader_test.go
@@ -0,0 +1,70 @@
+package loader_test
+
+import (
+	"context"
+	_ "embed"
+	"testing"
+
+	"github.com/friendly-fhir/fhenix/model/conformance"
+	"github.com/friendly-fhir/fhenix/model/loader"
+	"github.com/friendly-fhir/fhenix/registry"
+	"github.com/friendly-fhir/fhenix/registry/registrytest"
+)
+
+var (
+	//go:embed testdata/leaf-package/package.tar.gz
+	leafArchive []byte
+
+	//go:embed testdata/dependent-package-one/package.tar.gz
+	dependentArchiveOne []byte
+
+	//go:embed testdata/dependent-package-two/package.tar.gz
+	dependentArchiveTwo []byte
+)
+
+type TestListener struct {
+	loader.BaseListener
+}
+
+func (TestListener) OnLoad(ref registry.PackageRef) {
+
+}
+
+func TestLoader_Load(t *testing.T) {
+	const (
+		registryName = "test"
+		version      = "1.0.0"
+	)
+	var listener TestListener
+	client := registrytest.NewFakeClient()
+	client.SetOK("dependent.package.one", version, dependentArchiveOne)
+	client.SetOK("dependent.package.two", version, dependentArchiveTwo)
+	client.SetOK("leaf.package", version, leafArchive)
+
+	cache := registry.NewCache(t.TempDir())
+	cache.AddClient(registryName, client.Client)
+
+	downloader := registry.NewDownloader(cache)
+	downloader.Add(registryName, "dependent.package.one", version, true)
+	downloader.Add(registryName, "dependent.package.two", version, true)
+	if err := downloader.Start(context.Background()); err != nil {
+		t.Fatalf("downloader.Start() = %v, want nil", err)
+	}
+
+	module := conformance.DefaultModule()
+
+	loader := loader.New(cache,
+		loader.WithModule(module),
+		loader.WithWorkers(4),
+		loader.WithListeners(listener),
+	)
+
+	ref1 := registry.NewPackageRef(registryName, "dependent.package.one", version)
+	ref2 := registry.NewPackageRef(registryName, "dependent.package.two", version)
+
+	err := loader.Load(ref1, ref2)
+
+	if err != nil {
+		t.Errorf("loader.Load() = %v, want nil", err)
+	}
+}
